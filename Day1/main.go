211329367728663fea7a7b13e6ad206b200a812c
DamascenoV <victor.c.damasceno@gmail.com>
2023-12-01 23:24:02 +0000

    AoC Day 1 - Part 2

--- Day1/main.go
+++ Day1/main.go
@@ -8,43 +8,77 @@ import (
 	"strconv"
 )
 
+var numbers = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
 
 func main() {
-        content, err := os.Open("input.txt")
-        if err != nil {
-                panic(err)
-        }
+	content, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
 
-        readFile := bufio.NewScanner(content)
-        readFile.Split(bufio.ScanLines)
-        var lines []string
+	readFile := bufio.NewScanner(content)
+	readFile.Split(bufio.ScanLines)
+	var lines []string
 
-        for readFile.Scan() {
-                lines = append(lines, readFile.Text())
-        }
+	for readFile.Scan() {
+		lines = append(lines, readFile.Text())
+	}
 
-        content.Close()
+	content.Close()
 
-        var result int = 0
+	regexPartOne := "[1-9]"
+	regexPartTwo := "(one|two|three|four|five|six|seven|eight|nine|[1-9])"
 
-        for _, line := range lines {
-                number := regexp.MustCompile("[0-9]")
-                matches := number.FindAllString(line, -1)
+	partOne := result(lines, regexPartOne)
+	partTwo := result(lines, regexPartTwo)
 
-                valueCombine := matches[0] + matches[len(matches) -1]
-                value, err := strconv.Atoi(valueCombine)
+	fmt.Println(partOne)
+	fmt.Println(partTwo)
+}
 
-                if err != nil {
-                        panic(err)
-                }
+func result(lines []string, regex string) int {
+	var result int = 0
 
-                result = finalResult(&result, value)
-        }
+	for _, line := range lines {
+		number := regexp.MustCompile(regex)
+		matches := number.FindAllString(line, -1)
 
-        fmt.Println(result)
+		if len(matches) == 0 {
+			continue
+		}
+
+		valueCombine := convertSpelledNumber(matches[0]) + convertSpelledNumber(matches[len(matches)-1])
+		value, err := strconv.Atoi(valueCombine)
+
+		if err != nil {
+			panic(err)
+		}
+
+		result = finalResult(&result, value)
+	}
+
+	return result
 }
 
 func finalResult(result *int, value int) int {
-        *result += value
-        return *result
+	*result += value
+	return *result
+}
+
+func convertSpelledNumber(number string) string {
+	if _, ok := numbers[number]; ok {
+		return numbers[number]
+	}
+
+	return number
 }
